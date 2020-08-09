1677c4c9ad544d7a2f6e9cad6228f6afd8e134e7
juyeon <juyeonh@rainist.com>
2020-08-09 12:28:20 +0900

    Add test code for compact method

---
+++ __tests__/Array/compact.test.ts
@@ -0,0 +1,39 @@
+import { compact } from 'src/Array/compact';
+
+describe('Array', () => {
+  describe('01. compact', () => {
+    it('returns empty array if array passed as first param is empty', () => {
+      expect(compact([])).toStrictEqual([]);
+    });
+
+    it('returns new array (not a reference)', () => {
+      const inputArray: any[] = [];
+      const result = compact(inputArray);
+
+      expect(result).toStrictEqual([]);
+      expect(result).not.toBe(inputArray);
+    });
+
+    it('filters all falsy values', () => {
+      expect(
+        compact([
+          false,
+          0,
+          1.3,
+          null,
+          '',
+          'null',
+          undefined,
+          'world',
+          NaN,
+          6,
+        ]),
+      ).toStrictEqual([
+        1.3,
+        'null',
+        'world',
+        6,
+      ]);
+    });
+  });
+});
