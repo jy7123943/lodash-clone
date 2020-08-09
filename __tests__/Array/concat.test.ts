f4574b63f41db1fb192744cc85db70f3fa6fccb4
juyeon <juyeonh@rainist.com>
2020-08-09 14:00:30 +0900

    Remove nested describe method

--- __tests__/Array/concat.test.ts
+++ __tests__/Array/concat.test.ts
@@ -1,27 +1,25 @@
 import { concat } from 'src/Array/concat';
 
-describe('Array', () => {
-  describe('01. concat', () => {
-    it('returns empty array if array passed as first param is empty', () => {
-      expect(concat([])).toStrictEqual([]);
-    });
+describe('concat', () => {
+  it('returns empty array if array passed as first param is empty', () => {
+    expect(concat([])).toStrictEqual([]);
+  });
 
-    it('returns new array (not a reference)', () => {
-      const inputArray = [1, 2, 3];
-      const result = concat(inputArray);
+  it('returns new array (not a reference)', () => {
+    const inputArray = [1, 2, 3];
+    const result = concat(inputArray);
 
-      expect(result).toStrictEqual([1, 2, 3]);
-      expect(result).not.toBe(inputArray);
-    });
+    expect(result).toStrictEqual([1, 2, 3]);
+    expect(result).not.toBe(inputArray);
+  });
 
 
-    it('creates a concatenating array with additional values', () => {
-      expect(concat([1, 2, 3], 4, 5)).toStrictEqual([1, 2, 3, 4, 5]);
-    });
+  it('creates a concatenating array with additional values', () => {
+    expect(concat([1, 2, 3], 4, 5)).toStrictEqual([1, 2, 3, 4, 5]);
+  });
 
-    it('creates a concatenating array with additional array values', () => {
-      expect(concat([1, 2, 3], [4], [5])).toStrictEqual([1, 2, 3, 4, 5]);
-      expect(concat([1, 2, 3], [[4]], [5])).toStrictEqual([1, 2, 3, [4], 5]);
-    });
+  it('creates a concatenating array with additional array values', () => {
+    expect(concat([1, 2, 3], [4], [5])).toStrictEqual([1, 2, 3, 4, 5]);
+    expect(concat([1, 2, 3], [[4]], [5])).toStrictEqual([1, 2, 3, [4], 5]);
   });
 });
--- __tests__/Array/difference.test.ts
+++ __tests__/Array/difference.test.ts
@@ -1,40 +1,38 @@
 import { difference } from 'src/Array/difference';
 
-describe('Array', () => {
-  describe('difference', () => {
-    it('returns empty array if array passed as first param is not an array or empty', () => {
-      expect(difference([])).toStrictEqual([]);
-      expect(difference([], [2, 3])).toStrictEqual([]);
-    });
+describe('difference', () => {
+  it('returns empty array if array passed as first param is not an array or empty', () => {
+    expect(difference([])).toStrictEqual([]);
+    expect(difference([], [2, 3])).toStrictEqual([]);
+  });
 
-    it('returns copy of the first param if the second param is undefined', () => {
-      expect(difference([1, 2])).toStrictEqual([1, 2]);
-    });
+  it('returns copy of the first param if the second param is undefined', () => {
+    expect(difference([1, 2])).toStrictEqual([1, 2]);
+  });
 
-    it('returns copy of the first param if the second param is an empty array', () => {
-      expect(difference([1, 2], [])).toStrictEqual([1, 2]);
-    });
+  it('returns copy of the first param if the second param is an empty array', () => {
+    expect(difference([1, 2], [])).toStrictEqual([1, 2]);
+  });
 
-    it('returns new array (not a reference)', () => {
-      const inputArray = [1, 2, 3];
-      const result = difference(inputArray);
+  it('returns new array (not a reference)', () => {
+    const inputArray = [1, 2, 3];
+    const result = difference(inputArray);
 
-      expect(result).toStrictEqual([1, 2, 3]);
-      expect(result).not.toBe(inputArray);
-    });
+    expect(result).toStrictEqual([1, 2, 3]);
+    expect(result).not.toBe(inputArray);
+  });
 
-    it('creates an array of array values not included in the other given arrays', () => {
-      expect(difference([1, 2], [2, 3])).toStrictEqual([1]);
-      expect(difference(['a', null], ['a', null])).toStrictEqual([]);
-      expect(difference([undefined, 2], [2])).toStrictEqual([undefined]);
-    });
+  it('creates an array of array values not included in the other given arrays', () => {
+    expect(difference([1, 2], [2, 3])).toStrictEqual([1]);
+    expect(difference(['a', null], ['a', null])).toStrictEqual([]);
+    expect(difference([undefined, 2], [2])).toStrictEqual([undefined]);
+  });
 
-    it('checks a shallow equality for reference values', () => {
-      expect(difference([[2], 1], [[2]])).toStrictEqual([[2], 1]);
-      expect(difference(
-        [{ hello: 'world' }, 'string'],
-        [4, { hello: 'world' }],
-      )).toStrictEqual([{ hello: 'world' }, 'string']);
-    });
+  it('checks a shallow equality for reference values', () => {
+    expect(difference([[2], 1], [[2]])).toStrictEqual([[2], 1]);
+    expect(difference(
+      [{ hello: 'world' }, 'string'],
+      [4, { hello: 'world' }],
+    )).toStrictEqual([{ hello: 'world' }, 'string']);
   });
 });
