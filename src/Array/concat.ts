4b8298b1285fb02488e01a1a0447a1931c943d89
juyeon <juyeonh@rainist.com>
2020-08-09 12:49:38 +0900

    Add concat method

---
+++ src/Array/concat.ts
@@ -0,0 +1,23 @@
+/** concat(array, [values])
+ * Creates a new array concatenating array with any additional arrays and/or values.
+ * Arguments
+ * - array (Array): The array to concatenate.
+ * - [values] (...*): The values to concatenate.
+ * Returns - (Array): (Array): Returns the new concatenated array.
+ */
+
+export const concat = <T, K>(array: T[], ...values: (K[] | K[][])): (T | K | K[])[] => {
+  if (!Array.isArray(array) || !array.length) return [];
+
+  const result: (T | K | K[])[] = array.slice();
+
+  values.forEach((item: K | K[]) => {
+    if (Array.isArray(item)) {
+      result.push(item[0]);
+    } else {
+      result.push(item);
+    }
+  });
+
+  return result;
+};
