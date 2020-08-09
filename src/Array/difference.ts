9359f8caf55b05536bd53c0d0f18d7243f06eb4d
juyeon <juyeonh@rainist.com>
2020-08-09 13:58:18 +0900

    Add difference method

---
+++ src/Array/difference.ts
@@ -0,0 +1,15 @@
+/** difference(array, [values])
+ * Creates an array of array values not included in the other given arrays using SameValueZero for equality comparisons. The order and references of result values are determined by the first array.
+ * Note: Unlike 'pullAll', this method returns a new array.
+ * Arguments
+ * - array (Array): The array to inspect.
+ * - [values] (...Array): The values to exclude.
+ * Returns - (Array): Returns the new array of filtered values.
+ */
+
+export const difference = (array: any[], values?: any[]): any[] => {
+  if (!Array.isArray(array) || !array.length) return [];
+  if (!values || !Array.isArray(values) || !values?.length) return array.slice();
+
+  return array.filter(item => !values.includes(item));
+};
--- src/Array/index.ts
+++ src/Array/index.ts
@@ -1,9 +1,11 @@
 import { chunk } from './chunk';
 import { compact } from './compact';
 import { concat } from './concat';
+import { difference } from './difference';
 
 export {
   chunk,
   compact,
   concat,
+  difference,
 };
