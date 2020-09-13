0de90585c50de099e3191a98bb345f6b15a7fffd
juyeon <juyeonh@rainist.com>
2020-09-13 19:36:06 +0900

    Change test description

--- __tests__/Lang/isArrayLikeObject.test.ts
+++ __tests__/Lang/isArrayLikeObject.test.ts
@@ -3,13 +3,13 @@ import { isArrayLikeObject } from 'src/index';
 import { mockHTMLCollection } from '../utils/mock-html-collection';
 
 describe('isArrayLikeObject', () => {
-  it('returns true if parameter is considered array-like', () => {
+  it('returns true if parameter is considered array-like object', () => {
     expect(isArrayLikeObject(new Array(0))).toBe(true);
     expect(isArrayLikeObject([1, 2, 3])).toBe(true);
     expect(isArrayLikeObject(new (mockHTMLCollection as any)([1, 2, 3]))).toBe(true);
   });
 
-  it('returns false if parameter is not an array-like', () => {
+  it('returns false if parameter is not an array-like object', () => {
     expect(isArrayLikeObject('abc')).toBe(false);
     expect(isArrayLikeObject(1)).toBe(false);
     expect(isArrayLikeObject(undefined)).toBe(false);
