85b229449f87bb14f50c69829e99296ca424d50c
juyeon <juyeonh@rainist.com>
2021-03-14 16:23:55 +0900

    Add test case to test invoking the given functions

--- __tests__/Util/flow.test.ts
+++ __tests__/Util/flow.test.ts
@@ -1,10 +1,22 @@
 import { flow } from 'src/index';
 
 describe('flow', () => {
+  const add = (x: number, y: number) => x + y;
+  const doubles = (x: number) => x * 2;
+
   it('returns the new composite function', () => {
     const fn = jest.fn();
 
     expect(flow([fn])).not.toBe(fn);
   });
 
+  it('invokes functions in sequence with the return value of previous', () => {
+    const flowedFn = flow([add, doubles]);
+
+    const result = flowedFn(2, 3);
+
+    expect(add).toHaveBeenCalledWith(2, 3);
+    expect(doubles).toHaveBeenCalledWith(5);
+    expect(result).toBe(10);
+  });
 });
