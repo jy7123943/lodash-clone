1c0e6eea987e3d9ad2e26eaa1c1a588a40760cdb
juyeon <juyeonh@rainist.com>
2021-03-14 16:09:49 +0900

    Add flow function

---
+++ src/Util/flow.ts
@@ -0,0 +1,9 @@
+/** flow()
+ * Creates a function that returns the result of invoking the given functions with the this binding of the created function, where each successive invocation is supplied the return value of the previous.
+ * Arguments: [funcs] (...(Function|Function[])): The functions to invoke.
+ * Returns: (Function): Returns the new composite function.
+ */
+
+export const flow = () => {
+  // type here
+};
