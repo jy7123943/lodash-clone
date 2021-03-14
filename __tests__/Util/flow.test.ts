fc32d73d29933d5cd9c8e9ee1058161f8f423622
juyeon <juyeonh@rainist.com>
2021-03-14 17:23:33 +0900

    Add more test case with real functions

--- __tests__/Util/flow.test.ts
+++ __tests__/Util/flow.test.ts
@@ -2,7 +2,8 @@ import { flow } from 'src/index';
 
 describe('flow', () => {
   const add = (x: number, y: number) => x + y;
-  const doubles = (x: number) => x * 2;
+  const double = (x: number) => x * 2;
+  const tripple = (x: number) => x * 3;
 
   it('returns the new composite function', () => {
     const fn = jest.fn();
@@ -23,5 +24,13 @@ describe('flow', () => {
     expect(foo).toHaveBeenCalledWith(2, 3);
     expect(bar).toHaveBeenCalledWith('hello');
     expect(result).toBe('world');
+  });
+
+  it('returns 30 after invoking add, double and tripple', () => {
+    const flowedFn = flow([add, double, tripple]);
+
+    const result = flowedFn(2, 3);
+
+    expect(result).toBe(30);
   });
 });
