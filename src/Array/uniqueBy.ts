d5a65f2b0f00b6ecfb5f9ec7fedf40b2b55f96be
juyeon <juyeonh@rainist.com>
2021-03-24 11:02:40 +0900

    Add uniqueBy array method

---
+++ src/Array/uniqueBy.ts
@@ -0,0 +1,14 @@
+import { identity } from './../Util/identity';
+/** uniqBy(array, [iteratee=_.identity])
+ * This method is like _.uniq except that it accepts iteratee which is invoked for each element in array to generate the criterion by which uniqueness is computed. The order of result values is determined by the order they occur in the array. The iteratee is invoked with one argument:
+(value).
+ * Arguments
+ * - array (Array): The array to inspect.
+ * - [iteratee=_.identity] (Function): The iteratee invoked per element.
+ * Returns - (Array): Returns the new duplicate free array.
+ */
+
+type Func = (...params: any[]) => unknown;
+export const uniqueBy = <T>(array: T[], iteratee: Func = identity): T[] => {
+  return array;
+};
