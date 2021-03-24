29bc85f9f035f8f443497e7a809c427c97f90cd6
juyeon <juyeonh@rainist.com>
2021-03-24 13:57:52 +0900

    Add unique logic

--- src/Array/unique.ts
+++ src/Array/unique.ts
@@ -5,6 +5,26 @@
  * (Array): Returns the new duplicate free array.
  */
 
+const isSameValue = (x: any, y: any): boolean => {
+  if (typeof x !== typeof y) return false;
+  if (typeof x === 'number') {
+    if (isNaN(x)) return isNaN(y);
+  }
+
+  return x == y;
+};
+
+const key = (value: any): string => JSON.stringify(value);
+
 export const unique = <T>(array: T[]): T[] => {
-  return array;
+  const uniqSet: Record<string, { value: T }> = {};
+
+  return [...array].filter((value) => {
+    if (uniqSet[key(value)]) {
+      return !isSameValue(value, uniqSet[key(value)].value);
+    }
+    uniqSet[key(value)] = { value };
+
+    return true;
+  });
 };
