9c6df6ddf2664c693ac17ea408fd89e7ebaaad74
juyeon <juyeonh@rainist.com>
2021-03-24 10:58:38 +0900

    Add identity util method

---
+++ src/Util/identity.ts
@@ -0,0 +1,8 @@
+/** identity(value)
+ * This method returns the first argument it receives.
+ * Arguments
+ * - value (*): Any value.
+ * Returns - (*): Returns value.
+ */
+
+export const identity = <T>(param: T): T => param;
