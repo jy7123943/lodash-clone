e21bcc3cf512150524df43d70da5a4ed68c396df
juyeon <juyeonh@rainist.com>
2021-03-24 11:30:42 +0900

    Add unique test code

---
+++ __tests__/Array/unique.test.ts
@@ -0,0 +1,23 @@
+import { unique } from 'src/index';
+
+describe('unique', () => {
+  it('returns duplicate-free version of an array', () => {
+    expect(unique([2, 1, 2, 3, 1])).toEqual([2, 1, 3]);
+    expect(unique([NaN, +0, -0, null, NaN, undefined])).toEqual([NaN, 0, null, undefined]);
+    expect(unique(['string', true, true, 'string', 'str', false])).toEqual(
+      ['string', true, 'str', false],
+    );
+  });
+
+  it('makes a shallow comparison between reference values', () => {
+    expect(unique([[1, 2], { a: 1 }, [1, 2], { a: 1 }])).toEqual(
+      [[1, 2], { a: 1 }, [1, 2], { a: 1 }],
+    );
+
+    const obj = { a: 1 };
+    const arr = [1, 2];
+    expect(unique([obj, arr, obj, arr])).toEqual(
+      [obj, arr],
+    );
+  });
+});
