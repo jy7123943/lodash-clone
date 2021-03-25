35712998a3fa4c38470441824f9794782e54e416
juyeon <juyeonh@rainist.com>
2021-03-25 18:05:39 +0900

    Move isSameValue and key method as utils

--- src/Array/unique.ts
+++ src/Array/unique.ts
@@ -1,21 +1,11 @@
+import { isSameValue, key } from '../utils';
+
 /** uniq(array)
  * Creates a duplicate-free version of an array, using SameValueZero for equality comparisons, in which only the first occurrence of each element is kept. The order of result values is determined by the order they occur in the array.
  * Arguments
  * - array (Array): The array to inspect.
  * (Array): Returns the new duplicate free array.
  */
-
-const isSameValue = (x: any, y: any): boolean => {
-  if (typeof x !== typeof y) return false;
-  if (typeof x === 'number') {
-    if (isNaN(x)) return isNaN(y);
-  }
-
-  return x == y;
-};
-
-const key = (value: any): string => JSON.stringify(value);
-
 export const unique = <T>(array: T[]): T[] => {
   const uniqSet: Record<string, { value: T }> = {};
 
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,3 +7,14 @@ export const createIteratee = (
     ? (value: any) => (iteratee as Iteratee)(value)
     : (value: any) => value[iteratee as (string | number)]
 );
+
+export const isSameValue = (x: unknown, y: unknown): boolean => {
+  if (typeof x !== typeof y) return false;
+  if (typeof x === 'number') {
+    if (isNaN(x)) return isNaN(y as number);
+  }
+
+  return x == y;
+};
+
+export const key = (value: unknown): string => JSON.stringify(value);
