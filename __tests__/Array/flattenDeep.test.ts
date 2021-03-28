a04c7998b3cca587cafd672e8eab1eaf47abfcbb
Juyeon <jy7123943@gmail.com>
2021-03-28 16:39:43 +0900

    Add flattenDeep test code

--- __tests__/Array/flattenDeep.test.ts
+++ __tests__/Array/flattenDeep.test.ts
@@ -1,7 +1,22 @@
 import { flattenDeep } from 'src/index';
 
 describe('flattenDeep', () => {
-  it('', () => {
-    // Write here
+  it('returns new array', () => {
+    const param = [1, 2];
+    const result = flattenDeep(param);
+
+    expect(param).toEqual(result);
+    expect(param).not.toBe(result);
+  });
+
+  it('recursively flattens array', () => {
+    expect(flattenDeep([1, [2, [3, [4, [5]]]]])).toEqual([1, 2, 3, 4, 5]);
+    expect(flattenDeep([[{ a: 1 }, { a: 2 }], [{ a: 3 }, { a: 4 }]])).toEqual(
+      [{ a: 1 }, { a: 2 }, { a: 3 }, { a: 4 }],
+    );
+  });
+
+  it('sets undefined for empty slots in arrays', () => {
+    expect(flattenDeep([1, [2], , 4])).toEqual([1, 2, undefined, 4]);
   });
 });
