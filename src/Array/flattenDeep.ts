a2db7671f752605dae92456c27f6f7964627b528
Juyeon <jy7123943@gmail.com>
2021-03-28 16:47:05 +0900

    Add flattenDeep logic

--- src/Array/flattenDeep.ts
+++ src/Array/flattenDeep.ts
@@ -1,3 +1,4 @@
+import { isArray } from 'src/index';
 /** flattenDeep(array)
  * Recursively flattens array.
 
@@ -7,6 +8,15 @@
  * (Array): Returns the new flattened array.
 */
 
-export const flattenDeep = () => {
-  // Write here
+export const flattenDeep = <T>(array: (T | T[])[]): T[] => {
+  const result = [];
+  for (const item of array) {
+    if (isArray(item)) {
+      result.push(...flattenDeep(item));
+    } else {
+      result.push(item);
+    }
+  }
+
+  return result;
 };
