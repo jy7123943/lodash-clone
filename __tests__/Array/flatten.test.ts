238c276e59493338405b23b40c2cbadd5474b58b
Juyeon <jy7123943@gmail.com>
2021-03-28 12:06:54 +0900

    Add flatten test code

--- __tests__/Array/flatten.test.ts
+++ __tests__/Array/flatten.test.ts
@@ -1,7 +1,20 @@
 import { flatten } from 'src/index';
 
 describe('flatten', () => {
-  it('', () => {
-    // Write here
+  it('returns new array', () => {
+    const param = [1, 2];
+    const result = flatten(param);
+
+    expect(param).toEqual(result);
+    expect(param).not.toBe(result);
+  });
+
+  it('flattens array a single level deep', () => {
+    expect(flatten([1, [2, 3], [4, [5, 6]]])).toEqual([1, 2, 3, 4, [5, 6]]);
+    expect(flatten([[{ a: 1 }], { b: 2 }])).toEqual([{ a: 1 }, { b: 2 }]);
+  });
+
+  it('does not remove empty slots in arrays', () => {
+    expect(flatten([1, [2], , 4])).toEqual([1, 2, undefined, 4]);
   });
 });
