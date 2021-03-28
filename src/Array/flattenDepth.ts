35f6485d6bb049accaee632618efc98e542adc21
Juyeon <jy7123943@gmail.com>
2021-03-28 22:09:44 +0900

    Add flattenDepth logic

--- src/Array/flattenDepth.ts
+++ src/Array/flattenDepth.ts
@@ -1,4 +1,5 @@
-/** flattenDepth()
+import { isArray } from 'src/index';
+/** flattenDepth(array, [depth=1])
  * Recursively flatten array up to depth times.
 
  * Arguments
@@ -8,6 +9,23 @@
  * (Array): Returns the new flattened array.
 */
 
-export const flattenDepth = () => {
-  // Write here
+export const flattenDepth = <T>(array: (T | T[])[], depth = 1): (T | T[])[] => {
+  if (depth <= 0) {
+    return [...array];
+  }
+
+  const result = [];
+  for (const item of array) {
+    if (isArray(item)) {
+      if (depth > 1) {
+        result.push(...flattenDepth(item, depth - 1));
+      } else {
+        result.push(...item);
+      }
+    } else {
+      result.push(item);
+    }
+  }
+
+  return result;
 };
