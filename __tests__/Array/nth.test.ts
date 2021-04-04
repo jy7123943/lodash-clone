78150ce27a0d7c35a1524b15214c5d230fddbd53
Juyeon <jy7123943@gmail.com>
2021-04-04 23:49:14 +0900

    Change test code format

--- __tests__/Array/nth.test.ts
+++ __tests__/Array/nth.test.ts
@@ -4,17 +4,19 @@ describe('nth', () => {
   const array = [1, 2, 3, 4, 5];
 
   it('gets the element at index n of array and return it', () => {
-    array.forEach((expectedResult, index) => {
-      expect(nth(array, index)).toBe(expectedResult);
-    });
+    expect(nth(array, 0)).toBe(1);
+    expect(nth(array, 1)).toBe(2);
+    expect(nth(array, 2)).toBe(3);
+    expect(nth(array, 3)).toBe(4);
+    expect(nth(array, 4)).toBe(5);
   });
 
   it('gets the nth element from the end and return it', () => {
-    const expectedResults = [5, 4, 3, 2, 1];
-
-    array.forEach((_, index) => {
-      expect(nth(array, -(index + 1))).toBe(expectedResults[index]);
-    });
+    expect(nth(array, -1)).toBe(5);
+    expect(nth(array, -2)).toBe(4);
+    expect(nth(array, -3)).toBe(3);
+    expect(nth(array, -4)).toBe(2);
+    expect(nth(array, -5)).toBe(1);
   });
 
   it('returns undefined if n it greater than array length', () => {
