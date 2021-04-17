1e86a344907c077456a8f744f6da46ed78be2bef
Juyeon <jy7123943@gmail.com>
2021-04-17 15:38:11 +0900

    Define invoke parameters

--- src/Object/invoke.ts
+++ src/Object/invoke.ts
@@ -3,13 +3,17 @@
 
  * Arguments
  * - object (Object): The object to query.
- * - path (Array|string): The path of the method to invoke.
+ * - path (string): The path of the method to invoke.
  * - [args] (...*): The arguments to invoke the method with.
 
  * Returns
  * (*): Returns the result of the invoked method.
 */
 
-export const invoke = () => {
+export const invoke = <ObjectValue, InvokeArg, Result>(
+  obj: Record<string, ObjectValue>,
+  path: string,
+  ...args: InvokeArg[]
+): Result => {
   // Write here
 };
