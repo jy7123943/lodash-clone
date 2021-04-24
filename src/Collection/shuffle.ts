7ffbb6e9dd53ed382455502d6681a30a6d72e408
Juyeon <jy7123943@gmail.com>
2021-04-24 16:14:58 +0900

    Change push to unshift for shuffle method

--- src/Collection/shuffle.ts
+++ src/Collection/shuffle.ts
@@ -22,7 +22,7 @@ export const shuffle = <T>(collection: T[] | Record<string, T>): T[] => {
   while (values.length > 0) {
     const [randomValue] = values.splice(randomNum(0, values.length - 1), 1);
 
-    result.push(randomValue);
+    result.unshift(randomValue);
   }
 
   return result;
