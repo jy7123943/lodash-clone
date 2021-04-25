7a919631e8c023951f6a0fb45cee7d6916ecd2d4
Juyeon <jy7123943@gmail.com>
2021-04-25 14:30:29 +0900

    Add test case more complicated

--- __tests__/Array/zip.test.ts
+++ __tests__/Array/zip.test.ts
@@ -2,14 +2,16 @@ import { zip } from 'src/index';
 
 describe('zip', () => {
   it('returns a new array of grouped elements', () => {
-    expect(zip(['a', 'b'], [1, 2], [true, false])).toEqual([
+    expect(zip(['a', 'b', 'c'], [1, 2, 3], [true, false, true])).toEqual([
       ['a', 1, true],
       ['b', 2, false],
+      ['c', 3, true],
     ]);
 
-    expect(zip(['a', 'b'], [1], [true, false])).toEqual([
+    expect(zip(['a', 'b', 'c'], [1], [true, false])).toEqual([
       ['a', 1, true],
       ['b', undefined, false],
+      ['c', undefined, undefined],
     ]);
   });
 });
